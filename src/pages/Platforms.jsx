616f16de71c1b4cd4788db10c158533e83a17c1a
unknown <haritiana32.48@gmail.com>
2023-07-25 23:42:03 +0300

    amelioration de la page 'plateforme'

--- src/pages/Platforms.jsx
+++ src/pages/Platforms.jsx
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom";
 
 const Platforms = () => {
   return (
-    <div>
-      <div className="btnContainer">
+    <div className="btnContainer">
+      <img src="/images/LOGO-CEM.png" alt="" />
+      <p>
+        Bienvenue sur le guide et manuel d'utilisation des plateformes Ny Volako
+        et Sunupay
+      </p>
+      <p className="normalTxt">
+        Choisissez l'unes des plateformes auquel vous voulez avoir plus
+        d'information.
+      </p>
+      <div className="btns">
         <button className="btn btn-danger">
           <Link to={`/article/nyvolako`}>
             <div>Ny Volako</div>
