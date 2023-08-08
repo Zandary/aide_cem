7e611fe45d2fb76946b6260e8114388df64d3841
Zandary <haritiana32.48@gmail.com>
2023-08-08 15:38:33 +0300

    from laptop

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,46 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import data from "../../data.json";
+import nyvolako from "../../data.json";
+import sunupay from "../../data2.json";
 
 const Article = (props) => {
   const { id } = useParams();
+  const [data, setData] = useState({});
+  const [article, setArticle] = useState();
 
-  const article = data.article.find((a) => a.numero === id);
+  useEffect(() => {
+    if (props.platform === "nyvolako") {
+      setData(nyvolako);
+    } else {
+      setData(sunupay);
+    }
+
+    if (data.article) {
+      console.log(data !== {});
+      console.log("data  =", data);
+      setArticle(data.article.find((a) => a.numero === id));
+      console.log("article  =", article);
+    }
+  }, [data, id, props.platform, article]);
+
+  // console.log("out of useEffect   :", article.titre);
 
   return (
-    <div>
-      <h4>{article.titre}</h4>
-      <div>
-        {Object.keys(article.contenu).map((key) => (
-          <div key={key}>
-            {article.contenu[key].sousTitre &&
-            article.contenu[key].sousTitre !== "" ? (
-              <h5>
-                {article.contenu[key].numero +
-                  "- " +
-                  article.contenu[key].sousTitre}
-              </h5>
-            ) : (
-              ""
-            )}
+    <></>
+    // <div>
+    //   {article ? (
+    //     <>
+    //       <h4>{article.titre}</h4>
+    //       <div>
+    //         {Object.keys(article.contenu).map((key) => (
+    //           <div key={key}>
+    //             {article.contenu[key].sousTitre &&
+    //             article.contenu[key].sousTitre !== "" ? (
+    //               <h5>
+    //                 {article.contenu[key].numero +
+    //                   "- " +
+    //                   article.contenu[key].sousTitre}
+    //               </h5>
+    //             ) : (
+    //               ""
+    //             )}
 
-            {/* Vérifie si une image existe */}
-            {article.contenu[key].image !== "" ? (
-              <div>
-                <img
-                  src={`/images/${article.contenu[key].image}.png`}
-                  alt={key}
-                />
-              </div>
-            ) : (
-              ""
-            )}
-            <p>{article.contenu[key].texte}</p>
-            <br />
-          </div>
-        ))}
-      </div>
-    </div>
+    //             {/* Vérifie si une image existe */}
+    //             {article.contenu[key].image !== "" ? (
+    //               <div>
+    //                 <img
+    //                   src={`/images/${article.contenu[key].image}.png`}
+    //                   alt={key}
+    //                 />
+    //               </div>
+    //             ) : (
+    //               ""
+    //             )}
+    //             <p>{article.contenu[key].texte}</p>
+    //             <br />
+    //           </div>
+    //         ))}
+    //       </div>
+    //     </>
+    //   ) : (
+    //     "Nothing to display"
+    //   )}
+    // </div>
   );
 };
 
